187d52464d70edfb52d488c15f3a638575d2721c
huannguyen310 <huanchappie0809@gmail.com>
2024-06-03 14:23:42 +0700

    TH Bien so C#

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+//Khai bao bien
+int a;
+long b;
+float c;
+double d;
+decimal e;
+bool f;
+char g;
+string h;
+//Gan gia tri cho bien
+a = 10;
+b = 100000;
+c = 3.14f;
+d = 3.14;
+e = 10000000;
+f = true;
+g = 'H';
+h = "Hello World";
+//In ra man hinh
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
